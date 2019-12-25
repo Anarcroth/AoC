62b1448ddee75e5e46bbb2220a11dbfeb4c5302c
Martin Nestorov <mnestorov@protonmail.com>
2019-12-25 13:10:39 +0200

    Day 4, part 1, done

--- day4.clj
+++ day4.clj
@@ -5,18 +5,22 @@
   [passwd]
   (re-find #"(00|11|22|33|44|55|66|77|88|99)" passwd))
 
-(defn is-increasing
-  [passwd]
-  (for [[a b] (partition 2 passwd)]
-    (if (< (Integer/parseInt a) (Integer/parseInt b))
-      false
-      true)))
+(defn is-inc
+  [passwd i]
+  (if (>= (inc i) (count passwd))
+    true
+    (do
+      (def a (nth passwd i))
+      (def b (nth passwd (inc i)))
+      (if (<= (Integer/parseInt a) (Integer/parseInt b))
+        (is-inc passwd (inc i))
+        false))))
 
 (defn is-maybe-passwd
   [passwd]
   (if
       (and (are-same-adjecent (str passwd))
-           (is-increasing (clojure.string/split (str passwd) #"")))
+           (is-inc (clojure.string/split (str passwd) #"") 0))
     true
     false))
 
