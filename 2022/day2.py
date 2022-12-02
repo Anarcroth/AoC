552e0b6d034e38b361513a51f9331e92cc0457a7
Martin Nestorov <mnestorov@protonmail.com>
2022-12-02 07:58:00 +0200

    Day 2, Part 2

--- 2022/day2.py
+++ 2022/day2.py
@@ -92,3 +92,66 @@ with open('day2.txt') as f:
                 score = score + 6 + pts
 
 print(f'Part 1: {score}')
+
+"""
+--- Part Two ---
+
+The Elf finishes helping with the tent and sneaks back over to you. "Anyway,
+the second column says how the round needs to end: X means you need to lose, Y
+means you need to end the round in a draw, and Z means you need to win.
+Good luck!"
+
+The total score is still calculated in the same way, but now you need to figure
+out what shape to choose so the round ends as indicated. The example above now
+goes like this:
+
+* In the first round, your opponent will choose Rock (A), and you need the
+  round to end in a draw (Y), so you also choose Rock. This gives you a score
+  of 1 + 3 = 4.
+* In the second round, your opponent will choose Paper (B), and you choose Rock
+  so you lose (X) with a score of 1 + 0 = 1.
+* In the third round, you will defeat your opponent's Scissors with Rock for a
+  score of 1 + 6 = 7.
+
+Now that you're correctly decrypting the ultra top secret strategy guide, you
+would get a total score of 12.
+
+Following the Elf's instructions for the second column, what would your total
+score be if everything goes exactly according to your strategy guide?
+"""
+
+strat2 = {
+    'X': 'L',  # rock
+    'Y': 'D',  # paper
+    'Z': 'W',  # scissors
+}
+
+with open('day2.txt') as f:
+    rps = f.read().splitlines()
+    score = 0
+    for game in rps:
+        oponent = game[0]
+        me = strat2[game[2]]
+        if oponent == 'A':
+            if me == 'L':
+                score = score + 0 + 3
+            elif me == 'D':
+                score = score + 3 + 1
+            else:
+                score = score + 6 + 2
+        elif oponent == 'B':
+            if me == 'L':
+                score = score + 0 + 1
+            elif me == 'D':
+                score = score + 3 + 2
+            else:
+                score = score + 6 + 3
+        elif oponent == 'C':
+            if me == 'L':
+                score = score + 0 + 2
+            elif me == 'D':
+                score = score + 3 + 3
+            else:
+                score = score + 6 + 1
+
+print(f'Part 2: {score}')
