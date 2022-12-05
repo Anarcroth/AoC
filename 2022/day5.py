a86d72388ba034bcca8d123b36af269a4a1bc67d
Martin Nestorov <mnestorov@protonmail.com>
2022-12-05 07:55:18 +0200

    Day 5, Part 2

--- 2022/day5.py
+++ 2022/day5.py
@@ -121,3 +121,90 @@ def do_part1() -> str:
 
 
 print(f'Part 1: {do_part1()}')
+
+
+"""
+--- Part Two ---
+
+As you watch the crane operator expertly rearrange the crates, you notice the
+process isn't following your prediction.
+
+Some mud was covering the writing on the side of the crane, and you quickly
+wipe it away. The crane isn't a CrateMover 9000 - it's a CrateMover 9001.
+
+The CrateMover 9001 is notable for many new and exciting features: air
+conditioning, leather seats, an extra cup holder, and the ability to pick up
+and move multiple crates at once.
+
+Again considering the example above, the crates begin in the same
+configuration:
+
+    [D]
+[N] [C]
+[Z] [M] [P]
+ 1   2   3
+
+Moving a single crate from stack 2 to stack 1 behaves the same as before:
+
+[D]
+[N] [C]
+[Z] [M] [P]
+ 1   2   3
+
+However, the action of moving three crates from stack 1 to stack 3 means that
+those three moved crates stay in the same order, resulting in this new
+configuration:
+
+        [D]
+        [N]
+    [C] [Z]
+    [M] [P]
+ 1   2   3
+
+Next, as both crates are moved from stack 2 to stack 1, they retain their order
+as well:
+
+        [D]
+        [N]
+[C]     [Z]
+[M]     [P]
+ 1   2   3
+
+Finally, a single crate is still moved from stack 1 to stack 2, but now it's
+crate C that gets moved:
+
+        [D]
+        [N]
+        [Z]
+[M] [C] [P]
+ 1   2   3
+
+In this example, the CrateMover 9001 has put the crates in a totally different
+order: MCD.
+
+Before the rearrangement process finishes, update your simulation so that the
+Elves know where they should stand to be ready to unload the final supplies.
+After the rearrangement procedure completes, what crate ends up on top of each
+stack?
+"""
+
+
+def do_part2() -> str:
+    stacks = get_stacks()
+    with open('day5.txt') as f:
+        moves = f.read().splitlines()
+        for move in moves:
+            todo = move.split(' ')
+            how_much = int(todo[1])
+            source = stacks[int(todo[3])]
+            dest = stacks[int(todo[5])]
+            temp_dest = []
+            for r in range(how_much):
+                item = source.pop()
+                temp_dest.append(item)
+            temp_dest.reverse()
+            dest += temp_dest
+    return ''.join([column.pop() for stack, column in stacks.items()])
+
+
+print(f'Part 2: {do_part2()}')
